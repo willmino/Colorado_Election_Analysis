a18a4d26e75a5f0525605e9903623ba8e3c9de6d
willmino <will.mino@yahoo.com>
2022-11-29 14:18:32 -0800

    "uploading files"

---
+++ PythonPractice.py
@@ -0,0 +1,14 @@
+counties = ["Araphoe", "Denver", "Jefferson"]
+
+if counties[1] == "Denver":
+    print(counties[1])
+
+
+if "El Paso" in counties:
+
+    print("El Paso is in the list of counties.")
+
+else:
+
+    print("El Paso is not in the list of counties.")
+
