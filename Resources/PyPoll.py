3f3b737afae70fe318a27fd58c656c0886731dbf
willmino <will.mino@yahoo.com>
2022-11-30 10:43:40 -0800

    "Uploading Files"

---
+++ Resources/PyPoll.py
@@ -0,0 +1,13 @@
+# 1. The data we need to achieve
+
+# 2. The total number of votes cast
+
+# 3. The percentage of votes each candidate won.
+
+# 4. The total number of votes each candidate won.
+
+# 5. The winner of the election based on popular vote.
+
+# path to csv is
+# Resources/election_results.csv
+
